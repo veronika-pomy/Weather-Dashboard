8c0fbc15008122ee8a67e3129a3be360359a069e
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-01-04 18:57:42 -0500

    added pseudocode plan and example mockup image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,28 @@
-// Created new repo for weather app project.
-// Added comments
+// 1. Fetch weather data from the OpenWeather API
+    // 1.1. City name
+    // 1.2. Date
+    // 1.3. Icon for conditions (cloudy, sunny, rain, etc.)
+    // 1.4. Temp
+    // 1.5. Humidity 
+    // 1.6. Wind speed
+
+// 2. Save city name in local storage to keep the record of user history 
+
+// 3. HTML and CSS 
+    // 3.1. Title "Weather Dashboard" / "Get Weather" / "Know Weather"
+    // 3.2. Search Bar 
+    // 3.3. Btns to save and retrieve search history (city name should be the key)
+        // 3.3.1. Created dynamically in js
+        // 3.3.2. Everytime btn is clicked API fetches data for chosen city
+    // 3.4. Today's forecast for a given city 
+        // 3.4.1. Created dynamically in js
+        // 3.4.2. Data is updated when the city btn is clicked 
+    // 3.5. 5-day future forecast sections 
+        // 3.5.1 Created dynamically in js
+        // Data is updated when the city btn is clicked 
+
+
+
+
+
+    // See if I can use bootstrap and google apis
