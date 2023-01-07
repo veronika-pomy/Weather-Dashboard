3983f5bb59b4a8f853728cb8b9b95852097442a2
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-01-07 14:35:58 -0500

    js: solved retgetting data by lan and lon, accessed result vars to plug into html

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
 https://openweathermap.org/
 
 https://openweathermap.org/api/weathermaps
+
+https://openweathermap.org/forecast5
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,54 @@
 // store OpenWeather API key
-var WeatherApiKey = "d26e27c935d07fe0582289eb42ae5b1c";
+var weatherApiKey = "d26e27c935d07fe0582289eb42ae5b1c";
 // store user input for city 
-var userCity = "Detroit"; // may want to add state and country 
-// a query URL
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&appid=" + WeatherApiKey;
-// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-// get data using fetch
+var userCity = "Los Angeles";
+// a query URL to get lat and lon coordinates by city name
+var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${weatherApiKey}`;
 
+// get city coordinates using using fetch, then get weather report using those coordinates 
 function getWeather ( ) {
-    fetch(queryURL)
+    fetch(queryUrl)
     .then (function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
-    });
-};
+        var cityLat = data.coord.lat;
+        var cityLon = data.coord.lon;
+        // a query URL to get weather using city lat and lon coordinates 
+        // limit to getting data for today and a 5-day forcast 
+        // added imperial units to get temp in Fahrenheit and wind speed in MPH
+        var cityWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=${weatherApiKey}&units=imperial`;
+        
 
+            fetch(cityWeatherUrl)
+            .then (function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                var cityName = data.city.name;
+                console.log(cityName);
+                var forecastDate = new Date(data.list[0].dt_txt).toDateString();
+                console.log(forecastDate);
+                var cityConditon = data.list[0].weather[0].main;
+                console.log(cityConditon);
+                var cityTemp = `${Math.floor(data.list[0].main.temp)} °F`;
+                console.log(cityTemp);
+                var cityHum = `${data.list[0].main.humidity} %`;
+                console.log(cityHum);
+                var cityWindSpeed = `${data.list[0].wind.speed} MPH`;
+                console.log(cityWindSpeed);
+                // need for loop for a 5 dayforecast
+                // will need to figure out how to skip redundant timestamps
+            });
+    })};
+
+// list of conditions for icons
+// https://openweathermap.org/weather-conditions
+
+//  attach to onclick listener for all buttons 
 getWeather();
-
-// need to update to get the next 5 days too
-    // retreive geo coordinates for a given city name using https://openweathermap.org/api/weathermaps
-    // then pass into the forecast call
-
-    // 1.1. City name - yes
-    // 1.2. Date - yes
-        // try using this https://openweathermap.org/api/one-call-3
-    // 1.3. Conditions - yes (will need icon)
-    // 1.4. Temp - yes 
-        // uses kelvins, here is how to change https://openweathermap.org/api/one-call-3#data
-    // 1.5. Humidity - yes 
-    // 1.6. Wind speed - yes 
 
 // 2. Save city name in local storage to keep the record of user history 
 
-// 3. HTML and CSS 
-    // 3.1. Title "Weather Dashboard" / "Get Weather" / "Know Weather"
-    // 3.2. Search Bar 
-    // 3.3. Btns to save and retrieve search history (city name should be the key)
-        // 3.3.1. Created dynamically in js
-        // 3.3.2. Everytime btn is clicked API fetches data for chosen city
-    // 3.4. Today's forecast for a given city 
-        // 3.4.1. Created dynamically in js
-        // 3.4.2. Data is updated when the city btn is clicked 
-    // 3.5. 5-day future forecast sections 
-        // 3.5.1 Created dynamically in js
-        // Data is updated when the city btn is clicked 
-
-    // See if I can add bootstrap and googlefont apis
-
-    // make sure to add about in github
+    // make sure to add about in github !!!
